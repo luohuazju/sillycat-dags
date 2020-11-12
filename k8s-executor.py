31341d71bf76931926b49bd6ab3a338dbcc57465
Hua Luo <hluo@lifesize.com>
2020-11-11 22:35:20 -0600

    change module name

--- k8s-executor.py
+++ k8s-executor.py
@@ -4,7 +4,7 @@ This is an example dag for using the Kubernetes Executor.
 import os
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 
 args = {
